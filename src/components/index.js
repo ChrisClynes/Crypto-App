79fb51c9c32e1b65ffe78061cca504a5e6e99eda
Chris Clynes <chrisclynes@live.com>
2022-03-23 18:41:28 -0400

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Homepage } from './Homepage';// use to import a bunch of components to App.js file without cluttering App.js import.
+export { default as Navbar } from './Navbar';
+export { default as News } from './News';
+export { default as Cryptocurrencies } from './Cryptocurrencies';
+export { default as Exchanges } from './Exchanges';
+export { default as CryptoDetails } from './CryptoDetails';
