def657318c71be466a9adbde29a43b1f9529768a
Chris Clynes <chrisclynes@live.com>
2022-03-31 23:41:46 -0400

    added charts to Details page, need to fix line display of data.

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -6,7 +6,7 @@ import millify from 'millify';
 import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
-import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
 import LineChart from  './LineChart'
 
 const { Title, Text } = Typography;
@@ -16,8 +16,11 @@ const CryptoDetails = () => {
     const { coinId } = useParams();//takes the Id in useParams and allows you to use it as variable
     const [timePeriod, setTimePeriod] = useState('7d');
     const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
+    const { data: coinHistory } = useGetCryptoDetailsQuery({coinId, timePeriod})
     const cryptoDetails = data?.data?.coin;
     
+    if(isFetching) return "Loading..."
+
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
     const stats = [
